2e32dc50de0e9467b384a012b6cfd580d6631433
Douglas <douglaslundy@gmail.com>
2023-06-22 21:31:14 -0300

    created NavBar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "../../components/common/Header"
+import NavBar from '../../components/common/NavBar'
 import { Container } from './styled'
 const Home = () => {
   return (
     <Container>
       <Header />
+      <NavBar />
     </Container>
   )
 }
