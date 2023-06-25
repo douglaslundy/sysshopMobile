c08925092e90c690cd98c669e8708d02fb6257c4
Douglas <douglaslundy@gmail.com>
2023-06-25 17:50:00 -0300

    created product card

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -4,19 +4,17 @@ import { Container, Input, InputContainer, Logo, Search } from './styled'
 const logo = require("../../../../assets/images/horizontal-logo.png")
 const serach = require("../../../../assets/icons/search.png")
 
-export class Header extends Component {
-  render() {
-    return (
-      <Container>
-        <Logo source={logo} />
-        
-        <InputContainer>
-            <Input placeholder='Pesquisa' placeholderTextColor="#c0c0c1" />
-            <Search source={serach} />
-        </InputContainer>
-      </Container>
-    )
-  }
+const Header = () => {
+  return (
+    <Container>
+      <Logo source={logo} />
+
+      <InputContainer>
+        <Input placeholder='Pesquisa' placeholderTextColor="#c0c0c1" />
+        <Search source={serach} />
+      </InputContainer>
+    </Container>
+  )
 }
 
 export default Header
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from "../../components/common/Header"
 import NavBar from '../../components/common/NavBar'
 import { Container } from './styled'
+import ProductList from '../../components/common/ProductList'
 const Home = () => {
   return (
     <Container>
       <Header />
+      <ProductList />
       <NavBar />
     </Container>
   )
